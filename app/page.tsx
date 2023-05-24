aa0a6d4eb786a076983021591474a2ef3d0613e3
Hannah <76073097+hannahouazzane@users.noreply.github.com>
2023-05-24 16:23:26 +0100

    feat: add content

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,35 @@ export default function Home() {
   return (
     <>
       <PageHeader />
-      <main></main>;
+      <main>
+        <div>
+          <h1> Third sector unconference </h1>
+
+          <form>
+            <label>Sign up for more information</label>
+            <input type="email"></input>
+            <button>Submit</button>
+          </form>
+        </div>
+
+        <div>
+          <div>
+            <p>Location Icon</p>
+            <h2>DD/MM/YY Birmingham </h2>
+          </div>
+          <div>
+            <p>small circle</p>
+            <p>small circle</p>
+            <p>small circle</p>
+          </div>
+          <div>
+            <p>Twitter Icon</p>
+            <p>
+              Follow us on <a>Twitter </a> for updates
+            </p>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
