ab781778e436e3366427ec995d4e833296d98f87
Hannah <76073097+hannahouazzane@users.noreply.github.com>
2023-05-25 16:21:11 +0100

    style: create and apply styles to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,31 @@ export default function Home() {
       <PageHeader />
       <main>
         <div>
-          <h1> Third sector unconference </h1>
+          <h1 className="main-heading">
+            {" "}
+            Third sector <span className="main-heading-red-letters">un</span>
+            conference{" "}
+          </h1>
 
-          <form>
-            <label>Sign up for more information</label>
-            <input type="email"></input>
-            <button>Submit</button>
+          <form className="sign-up-form">
+            <label className="sign-up-label">
+              Sign up for more information
+            </label>
+            <div>
+              <input
+                className="sign-up-input-field"
+                type="email"
+                placeholder="example@mail.com"
+              ></input>
+              <button className="sign-up-button">Submit</button>
+            </div>
           </form>
         </div>
 
         <div>
           <div>
             <p>Location Icon</p>
-            <h2>DD/MM/YY Birmingham </h2>
+            <h2 className="date-heading">DD/MM/YY Birmingham </h2>
           </div>
           <div>
             <p>small circle</p>
@@ -26,9 +38,9 @@ export default function Home() {
             <p>small circle</p>
           </div>
           <div>
-            <p>Twitter Icon</p>
-            <p>
-              Follow us on <a>Twitter </a> for updates
+            <p> Twitter Icon</p>
+            <p className="twitter-cta-text">
+              Follow us on <a className="twitter-link">Twitter</a> for updates
             </p>
           </div>
         </div>
