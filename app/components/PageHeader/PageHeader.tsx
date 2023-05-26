34e14c221412c91dad58c244cb1062afc3f031dd
Hannah <76073097+hannahouazzane@users.noreply.github.com>
2023-05-26 12:02:56 +0100

    style: add logo and header styles

--- app/components/PageHeader/PageHeader.tsx
+++ app/components/PageHeader/PageHeader.tsx
@@ -1,10 +1,21 @@
+import Image from "next/image";
 import "./PageHeader.css";
+
 const PageHeader = () => {
   return (
     <header>
-      <div className="logo-container">Logo</div>
+      <div className="logo-container">
+        <Image alt="not working" src="logo.svg" width={250} height={51} />
+      </div>
 
-     
+      <div>
+        <a
+          className="privacy-link"
+          href="https://www.dxw.com/privacy-statement/"
+        >
+          Privacy Policy
+        </a>
+      </div>
     </header>
   );
 };
