b5aa1f08c44d963064b4d59b50656784899f5f88
Hannah <76073097+hannahouazzane@users.noreply.github.com>
2023-06-06 15:09:37 +0100

    style: position twitter container

--- app/page.tsx
+++ app/page.tsx
@@ -104,6 +104,27 @@ export default function Home() {
                 <button className="sign-up-button">Submit</button>
               </div>
             </form>
+
+            <div className="twitter-mobile-container">
+              <Image
+                className="twitter-image"
+                src="twitter-icon.svg"
+                width={35}
+                height={35}
+                alt="Twitter"
+              />
+
+              <p className="twitter-cta-text">
+                Follow us on{" "}
+                <a
+                  href="https://twitter.com/ukcharitycamp"
+                  className="twitter-link"
+                >
+                  Twitter
+                </a>{" "}
+                for updates
+              </p>
+            </div>
           </div>
 
           <div className="right-container">
